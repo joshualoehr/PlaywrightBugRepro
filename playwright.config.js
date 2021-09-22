e996b264a5b017ce98f81e1d9016ebd7bacf4c37
Josh Loehr <josh.loehr@smartsheet.com>
2021-09-22 15:06:25 -0700

    Add dev server to config

---
+++ playwright.config.js
@@ -0,0 +1,10 @@
+const config = {
+    webServer: {
+        command: "yarn server",
+        port: 3001,
+        timeout: 5000,
+        reuseExistingServer: true
+    },
+};
+
+module.exports = config;
